8b43d6cdfa31ea7527170d3a85604ea6dc548e40
AlphaZ <volodimir.pilipets123@gmail.com>
2021-12-05 17:55:35 +0200

    Initial

---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,90 @@
+﻿
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+	
+	public delegate void PlayerDelegate();
+	public static event PlayerDelegate OnPlayerDied;
+	public static event PlayerDelegate OnPlayerScored;
+	public Animator animator;
+	public float tapForce = 10;
+	public float tiltSmooth = 5;
+	public Vector3 startPos;
+	public AudioSource tapSound;
+	public AudioSource scoreSound;
+	public AudioSource dieSound;
+	public ParticleSystem particle;
+	Rigidbody2D rigidBody;
+	Quaternion downRotation;
+	Quaternion forwardRotation;
+	public bool isDead;
+	GameManager game;
+	public void set_isDead(bool value)
+    {
+		isDead = value;
+    }
+	void Start() {
+		rigidBody = GetComponent<Rigidbody2D>();
+		downRotation = Quaternion.Euler(0, 0 ,-100);
+		forwardRotation = Quaternion.Euler(0, 0, 40);
+		game = GameManager.Instance;
+		rigidBody.simulated = false;
+		animator = gameObject.GetComponent<Animator>();
+		set_isDead(false);
+	}
+
+	void OnEnable() {
+		GameManager.OnGameStarted += OnGameStarted;
+		GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+	}
+
+	void OnDisable() {
+		GameManager.OnGameStarted -= OnGameStarted;
+		GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+	}
+
+	void OnGameStarted() {
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.simulated = true;
+
+	}
+
+	void OnGameOverConfirmed() {
+		transform.localPosition = startPos;
+		transform.rotation = Quaternion.identity;
+
+	}
+
+	void Update()
+	{
+		animator.SetBool("isDead", isDead);
+		if (game.GameOver) return; 
+		if (Input.GetMouseButtonDown(0)) {
+			rigidBody.velocity = Vector2.zero;
+			transform.rotation = forwardRotation;
+			rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+			particle.Play();
+			animator.SetTrigger("Jump");
+			tapSound.Play();
+		}
+		if (!isDead) animator.SetTrigger("Idle");
+		else animator.Play("Die");
+		transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "ScoreZone") {
+			OnPlayerScored();
+			scoreSound.Play();
+		}
+		if (col.gameObject.tag == "DeadZone") {
+			rigidBody.simulated = false;
+			OnPlayerDied();
+			set_isDead(true);
+			animator.StopPlayback();
+			animator.SetBool("isDead", isDead);
+			dieSound.Play();
+		}
+	}
+
+}
---
+++ Assets/scripts/backgroundScroller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class backgroundScroller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private float width;
+    public float speed = -2f;
+    private Vector2 startingPoint;
+    GameManager game;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = bc.size.x;
+        bc.enabled = false;
+        rb.velocity = new Vector2(0, 0);
+        game = GameManager.Instance;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!game.GameOver)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+            else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        if(transform.position.x < -width)
+        {
+            transform.position = new Vector2(width * Mathf.Abs(transform.localScale.x), 0);// startingPoint;// (Vector2)transform.position + new Vector2(width * 2, 0);
+        }
+    }
+}
